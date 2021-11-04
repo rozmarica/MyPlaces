5c2aa41b21d851deaa954779a4d07c7b9aa3fd76
rozmarica luiza <rozmarica.luiza@gmail.com>
2021-11-04 20:02:21 +0400

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Луиза on 04.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
